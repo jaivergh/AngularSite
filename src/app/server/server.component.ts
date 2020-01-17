afeb54da13caa42c7d1e0651025ce6d817c847eb
jcamilogh <jcamilogh@outlook.com>
2020-01-16 20:29:00 -0500

    Modify server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,10 +9,31 @@ import { Server } from '../shared/server';
 export class ServerComponent implements OnInit {
 
   constructor() { }
+
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
 
   ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean){
+    this.serverInput.isOnline = isOnline;
+    if (isOnline) {
+      this.color = '#66BB6a';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';      
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus );
+    this.setServerStatus(!onlineStatus);
   }
 
 }
